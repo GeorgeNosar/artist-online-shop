3bfe7770ec0a035687e9f09ce760d6fc9f268913
ov3r <georgynosar@gmail.com>
2018-01-24 23:16:08 +0300

    Added: carousel, logo, radius, icons

--- js/common.js
+++ js/common.js
@@ -3,3 +3,14 @@ $(".popup").magnificPopup(
   removalDelay: 350,
   mainClass: 'mfp-fade'
 });
+
+$(".carousel").owlCarousel({
+		loop:true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: false,
+		autoplaySpeed: 700,
+		items: 1,
+		mouseDrag: false,
+		dots: false,
+	});
